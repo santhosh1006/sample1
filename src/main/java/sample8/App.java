532ee624d871dff5cc8f41467cab1b95160f474f
santhosh <baddamsanthosh1006@gmail.com>
2019-03-21 21:49:08 +0530

    1

---
+++ src/main/java/sample8/App.java
@@ -0,0 +1,14 @@
+package sample8;
+public class App {
+
+    public static void main(String[] args) {
+        int rows = 5;
+
+        for(int i = rows; i >= 1; --i) {
+            for(int j = 1; j <= i; ++j) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
